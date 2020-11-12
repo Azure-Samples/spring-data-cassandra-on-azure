f5e51df0b5e9c8c7c75f0a06bd4ab5a9f6de85a2
wujack778 <69495791+wujack778@users.noreply.github.com>
2020-11-12 14:33:07 +0800

    Explanation update (#2)
    * explanation update for class PetController.java
    
    * explanation update for class PetController.java

--- src/main/java/com/microsoft/azure/samples/spring/PetController.java
+++ src/main/java/com/microsoft/azure/samples/spring/PetController.java
@@ -18,9 +18,18 @@ import java.util.UUID;
 @Controller
 @RequestMapping(path="/pets")
 public class PetController {
+
+    /**
+     * using spring data source defined in application.properties,autowire petrepository as an entity.
+     */
     @Autowired
     private PetRepository petRepository;
 
+    /**
+     * createPet used the spring framework to create the sql statement using the method save(...).
+     * @param pet
+     * @return  String like "Added ......"
+     */
     @PostMapping
     public @ResponseBody String createPet(@RequestBody Pet pet) {
         pet.setId(UUIDs.timeBased());
@@ -28,16 +37,30 @@ public class PetController {
         return String.format("Added %s.", pet);
     }
 
+    /**
+     * getAllPets used the spring framework to create the sql statement using the method findAll().
+     * @return all pets you created
+     */
     @GetMapping
     public @ResponseBody Iterable<Pet> getAllPets() {
         return petRepository.findAll();
     }
 
+    /**
+     * getPet used the spring framework to create the sql statement using the method findById(...)
+     * @param id
+     * @return  the pet by id
+     */
     @GetMapping("/{id}")
     public @ResponseBody Optional<Pet> getPet(@PathVariable UUID id) {
         return petRepository.findById(id);
     }
 
+    /**
+     * deletePet used the spring framework to create the sql statement using the method deleteById(...)
+     * @param id
+     * @return  String like "Deleted (id)"
+     */
     @DeleteMapping("/{id}")
     public @ResponseBody String deletePet(@PathVariable UUID id) {
         petRepository.deleteById(id);
